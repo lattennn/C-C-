fda4d69d03d67c110d5c48a6b5ec98115bd6d49a
lattennn <48510816+lattennn@users.noreply.github.com>
2020-08-06 21:20:39 +0200

    Update and rename 3.FinobbNumber.c to 3.FibonacciNumbers.c

--- 1 Basic & Structure & Pointer/3.FibonacciNumbers.c
+++ 1 Basic & Structure & Pointer/3.FibonacciNumbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+//0 1 1 2 3 5 8...
 //1 2 3 5 8....
 int main()
 {
     int a[20];//memory allocation is already done,dimension is equal to 20
     int i;
 
-    a[0]=1;
-    a[1]=2;
-    printf("1 2 ");
+    a[0]=0;
+    a[1]=1;
+    printf("0 1 ");
     for(i=2;i<20;i++){ //iteration
         a[i]=a[i-1]+a[i-2];
         printf(" %d",a[i]);
